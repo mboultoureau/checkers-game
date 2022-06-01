1c71ac6cf0edcda32b377299f774f535c78149e7
mboultoureau <contact@mboultoureau.com>
2022-06-01 17:25:57 +0200

    Added board and pawn classes

---
+++ code/src/Board.java
@@ -0,0 +1,76 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+
+    public static int COLUMNS = 10;
+    public static int ROWS = 10;
+
+    private List<List<Pawn>> board;
+
+    public Board() {
+        this.board = new ArrayList<List<Pawn>>(ROWS);
+        this.initBoard();
+    }
+
+    public void clearBoard() {
+        for (int row = 0; row < ROWS; row++) {
+            this.board.get(row).clear();
+        }
+        this.board.clear();
+    }
+
+    public void initBoard() {
+        // Initial creation of the game board
+        for (int row = 0; row < ROWS; row++) {
+            ArrayList<Pawn> newRow = new ArrayList<Pawn>(COLUMNS);
+
+            for (int column = 0; column < COLUMNS; column++) {
+                // 4 first rows
+                if (row < 4) {
+                    if ((column % 2 != 0 && row % 2 == 0) || (column % 2 == 0 && row % 2 != 0)) {
+                        newRow.add(new Pawn(Pawn.PAWN_COLOR.BLACK, Pawn.PAWN_TYPE.PAWN));
+                    } else {
+                        newRow.add(null);
+                    }
+                }
+
+                // 4 last rows
+                else if (row > 5) {
+                    if ((column % 2 != 0 && row % 2 == 0) || (column % 2 == 0 && row % 2 != 0)) {
+                        newRow.add(new Pawn(Pawn.PAWN_COLOR.WHITE, Pawn.PAWN_TYPE.PAWN));
+                    } else {
+                        newRow.add(null);
+                    }
+                }
+
+                // 2 center paws
+                else {
+                    newRow.add(null);
+                }
+            }
+
+            this.board.add(newRow);
+        }
+    }
+
+    public void debugBoard() {
+        for (int row = 0; row < ROWS; row++) {
+            for (int column = 0; column < COLUMNS; column++) {
+                if (this.board.get(row).get(column) != null) {
+                    switch (this.board.get(row).get(column).getColor()) {
+                        case WHITE -> System.out.print("W ");
+                        case BLACK -> System.out.print("B ");
+                        default -> System.out.print("? ");
+                    }
+                } else {
+                    System.out.print(". ");
+                }
+            }
+
+            System.out.print("\n");
+        }
+    }
+
+}
---
+++ code/src/Pawn.java
@@ -0,0 +1,28 @@
+public class Pawn {
+    public enum PAWN_COLOR { BLACK, WHITE };
+    public enum PAWN_TYPE { PAWN, QUEEN };
+
+    private PAWN_COLOR color;
+    private PAWN_TYPE type;
+
+    public Pawn(PAWN_COLOR color, PAWN_TYPE type) {
+        this.color = color;
+        this.type = type;
+    }
+
+    public PAWN_COLOR getColor() {
+        return color;
+    }
+
+    public void setColor(PAWN_COLOR color) {
+        this.color = color;
+    }
+
+    public PAWN_TYPE getType() {
+        return type;
+    }
+
+    public void setType(PAWN_TYPE type) {
+        this.type = type;
+    }
+}
