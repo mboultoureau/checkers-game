8d51e859a7e2a873de6669d638f72dac4d27feb7
ronanren <renouxpro@gmail.com>
2022-06-07 12:06:29 +0200

    update menu

--- code/src/main/java/view/BoardView.java
+++ code/src/main/java/view/BoardView.java
@@ -15,11 +15,16 @@ public class BoardView extends JFrame {
 
     private JLabel background;
     private Board board;
+    private Button playButton;
+    private TextField textUser1;
+    private TextField textUser2;
+    private JLabel labelUser1;
+    private JLabel labelUser2;
 
     public BoardView(Board board) {
         this.board = board;
         this.setTitle("Checkers Game");
-        this.setSize(600, 600);
+        this.setSize(700, 530);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         this.display();
@@ -28,9 +33,24 @@ public class BoardView extends JFrame {
 
     public void display() {
         JPanel layout = new JPanel();
-        layout.setLayout(new GridLayout(1, 1));
+        playButton = new Button("Play");
+        textUser1 = new TextField();
+        textUser2 = new TextField();
+        labelUser1 = new JLabel("Name of player 1 :");
+        labelUser2 = new JLabel("Name of player 2 :");
 
-        layout.add(new GridView());
+        if (false) {
+            layout.setLayout(new GridLayout(1, 1));
+            layout.add(new GridView());
+        } else {
+            this.setSize(450, 200);
+            layout.setLayout(new GridLayout(3, 2, 20, 20));
+            layout.add(labelUser1);
+            layout.add(textUser1);
+            layout.add(labelUser2);
+            layout.add(textUser2);
+            layout.add(playButton);
+        }
 
         this.setContentPane(layout);
     }
